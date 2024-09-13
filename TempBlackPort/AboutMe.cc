9187ccd6198e8d05d93f80116d1ebf4faff99ce6
shahanxali <shahanali252002@gmail.com>
2024-09-13 16:45:03 +0530

    commit fin

---
+++ TempBlackPort/AboutMe.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string question;
+    cin >> question;
+
+    if(question == "Who am I?"){ 
+        cout << "I'm Shahan Ali, and I like to think of myself as a proud purveyor of the fine art of problem-solving.
+                So, let's dig deeper. What really gets my gears turning is tackling puzzles,
+                learning new things, and embracing challenges head-on." << end;
+
+    }
+
+    else if(question == "What I do?"){
+        cout << "I am a B Tech computer Science and Engineering student with major specialization of Gaming and Graphics and minor with
+                Artificial Intelligence, I am up for a high quality tech job and learning opportunities, I wanna grab as much skill as
+                I can and be on top of the programmers someday." << endl;
+    }
+
+    else if(question == "What I like?"){
+        cout << "Things i Like are:
+                1- Football, I have played district level under 19 form the district Moradabad and I like football since i was a kid
+                2- Reading, I like reading either be it fiction or non fiction, reading is waht interests me and make me a better human
+                3- Music, Music is an art not gonna lie, I love songs mostly classical hindi
+                4- Chess, Nobody can ever be perfect in this game but one can be better from there opponent,so yeah I like new challenges." << endl;
+
+    }
+
+    return 0;
+}
